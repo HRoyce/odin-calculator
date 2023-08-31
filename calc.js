5427f5626b857e19e231d83c42ba8e13da01c85e
Hunter Musselman <120432208+HRoyce@users.noreply.github.com>
2023-08-30 21:36:35 -0500

    Created functions for each operation, fixed the boolean which checks for zero. Made it so each button updates a variable which holds the number, consolidating the callback to a single function, and updating the div content with the variable, rather than appending. Need to fix the clear button which currently isn't working.

--- calc.js
+++ calc.js
@@ -1,12 +1,3 @@
-//How I would improve this code:
-//Add a prop with the selector which converts the
-// value of the button into its integer value
-//as a prop, then uses it to as an argument
-//for the click function which would allow me to
-//reuse a function and use one loop
-//to assign action listeners saving alot of space
-
-
 //Where I left off: 
 
 //make sure numbers stay inside of screen
@@ -26,83 +17,67 @@ const screen = document.getElementById("displayScreen");
 let displayValue = document.getElementById("displayValue");
 //resolve a simple equation (1 operator)
 let displayNum = "";
-const zero = document.getElementById("0");
-const one = document.getElementById("1");
-const two = document.getElementById("2");
-const three = document.getElementById("3");
-const four = document.getElementById("4");
-const five = document.getElementById("5");
-const six = document.getElementById("6");
-const seven = document.getElementById("7");
-const eight = document.getElementById("8");
-const nine = document.getElementById("9");
+const Zero = document.getElementById("0");
+const One = document.getElementById("1");
+const Two = document.getElementById("2");
+const Three = document.getElementById("3");
+const Four = document.getElementById("4");
+const Five = document.getElementById("5");
+const Six = document.getElementById("6");
+const Seven = document.getElementById("7");
+const Eight = document.getElementById("8");
+const Nine = document.getElementById("9");
 
-const clrButton = document.getElementById("clr");
-const addButton = document.getElementById("add");
-const subButton = document.getElementById("sub");
-const mulButton = document.getElementById("mul");
-const divButton = document.getElementById("div");
-const eqbutton = document.getElementById("eq");
+const ClrButton = document.getElementById("clr");
+const AddButton = document.getElementById("add");
+const SubButton = document.getElementById("sub");
+const MulButton = document.getElementById("mul");
+const DivButton = document.getElementById("div");
+const Eqbutton = document.getElementById("eq");
 
-let nonZero = false;
-zero.addEventListener('click', () => {
-    if (nonZero == false){ }
-    else{
-        displayValue.textContent += "0";
+function add(a, b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a/b;
+}
+let num1 = "0";
+let num2 = "";
+let numIsZero = true; //prevents leading zero
+function updateNum(a){
+    if(a == "0"){
+        if(numIsZero){return;}
+        
     }
-    console.log(displayNum);
-})
-one.addEventListener('click',()=>{
-    displayValue.textContent+= "1";
-    console.log(displayNum);
-    nonZero = true;
-})
-two.addEventListener('click',()=>{
-    displayValue.textContent+="2";
-    console.log(displayNum);
-    nonZero = true;
-})
-three.addEventListener('click',()=>{
-    displayValue.textContent+="3";
-    console.log(displayNum);
-    nonZero = true;
-})
-four.addEventListener('click',()=>{
-    displayValue.textContent+="4";
-    console.log(displayNum);
-    nonZero = true;
-})
-five.addEventListener('click',()=>{
-    displayValue.textContent+="5";
-    console.log(displayNum);
-    nonZero = true;
-})
-six.addEventListener('click',()=>{
-    displayValue.textContent+="6";
-    console.log(displayNum);
-    nonZero = true;
-})
-seven.addEventListener('click',()=>{
-    displayValue.textContent+="7";
-    console.log(displayNum);
-    nonZero = true;
-})
-eight.addEventListener('click',()=>{
-    displayValue.textContent+="8";
-    console.log(displayNum);
-    nonZero = true;
-})
-nine.addEventListener('click',()=>{
-    displayValue.textContent+="9";
-    console.log(displayNum);
-    nonZero = true;
-})
+    if(a == "~"){
+        num1 ="0"; 
+        numIsZero = true; 
+        displayValue.textContent = num1;
+        return;}
+    
+    num1 += a;
+    displayValue.textContent = num1;
+    numIsZero = false;
+}
 
-clrButton.addEventListener('click',()=>{
-    displayValue.textContent="";
-    console.log(displayNum);
-    nonZero = false;
+Zero.addEventListener('click', ()=>{updateNum("0")})
+One.addEventListener('click', ()=>{updateNum("1")})
+Two.addEventListener('click', ()=>{updateNum("2")})
+Three.addEventListener('click', ()=>{updateNum("3")})
+Four.addEventListener('click', ()=>{updateNum("4")})
+Five.addEventListener('click', ()=>{updateNum("5")})
+Six.addEventListener('click', ()=>{updateNum("6")})
+Seven.addEventListener('click', ()=>{updateNum("7")})
+Eight.addEventListener('click', ()=>{updateNum("8")})
+Nine.addEventListener('click', ()=>{updateNum("9")})
 
-})
+clrButton.addEventListener('click', ()=>{updateNum("~")})
+
 
 
